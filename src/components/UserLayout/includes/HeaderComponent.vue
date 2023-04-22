86101bd84e3a6ceb0381d606dfacf4d2fae673d5
westcorperations <onwusioneejikek04207>
2023-04-22 20:43:21 +0100

    *header and jhow to page done

--- src/components/UserLayout/includes/HeaderComponent.vue
+++ src/components/UserLayout/includes/HeaderComponent.vue
@@ -1,12 +1,81 @@
 <template>
     <div>
-        <header class="bg-primary-100">
-            header
-        </header>
+        <header>
+
+<!-- Section: header -->
+<section class="mb-20">
+<NavBarComponent />
+ 
+
+  <div class="relative overflow-hidden bg-no-repeat bg-cover" style="background-position: 50%; background-image: url('https://drive.google.com/uc?export=download&id=17uPLO7CY5ICgbkGwA2UZNXzpc9USXd5F'); height: 500px;">
+    <div class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
+          style="background-color: rgba(0, 0, 0, 0.75)">
+          <div class="flex justify-center items-center h-full">
+            <div class="text-center text-white px-6 py-6 md:py-0 md:px-12 max-w-[800px]">
+              <h2 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight leading-tight mb-12">
+                Are you ready <br /><span>for an adventure</span>
+              </h2>
+              <p class="text-lg">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima officia
+                consequatur adipisci tenetur repudiandae rerum quos.
+              </p>
+            </div>
+          </div>
+        </div>
+
+</div>
+
+  <div class="container mx-auto px-6 md:px-12 xl:px-32">
+    <div class="text-center text-gray-800">
+      <div class="grid lg:grid-cols-5 grid-cols-1 rounded-lg shadow-lg px-6 py-12 md:py-16 md:px-12" style="margin-top: -60px; background: hsla(0, 0%, 100%, 0.7); backdrop-filter: blur(30px);">
+        <div class="flex flex-col p-2 lg:border-r-2  border-gray-300">
+            <label for="motor" class="text-left px-2 text-gray-400 ">Choose Bus</label>
+            <select name="" id="" class="border-0 px-1 focus:border-primary-50">
+                <option value="">volvo</option>
+                <option value="">toyota</option>
+                <option value="">hyanda</option>
+            </select>
+        </div>
+        <div class="flex flex-col p-2 lg:border-r-2  border-gray-300">
+            <label for="motor" class="text-left px-2 text-gray-400 ">Available Time</label>
+            <select name="" id="" class="border-0 px-1 focus:border-primary-50">
+                <option value="">10:30am</option>
+                <option value="">12:00pm</option>
+             
+            </select>
+        </div>
+        <div class="flex flex-col p-2 lg:border-r-2  border-gray-300">
+            <label for="motor" class="text-left px-2 text-gray-400 ">Choose Sit </label>
+            <select name="" id="" class="border-0 px-1 focus:border-primary-50">
+                <option value="">10:30am</option>
+                <option value="">12:00pm</option>
+             
+            </select>
+        </div>
+        <div class="flex flex-col p-2 lg:border-r-2  border-gray-300">
+            <label for="motor" class="text-left px-2 text-gray-400 ">Price </label>
+            <select name="" id="" class="border-0 px-1 focus:border-primary-50">
+                <option value="">NGN2000</option>
+                <option value="">NGN500</option>
+             
+            </select>
+        </div>
+        
+        <div class="py-4 px-2">
+            <button class="bg-primary-100 text-white px-10 py-2 rounded-lg hover:bg-primary-50 ">Book</button>
+        </div>
+        </div>
+    </div>
+  </div>
+</section>
+<!-- Section: header -->
+
+</header>
     </div>
 </template>
 
 <script setup>
+import NavBarComponent from '@/components/UserLayout/includes/NavBarComponent.vue'
 
 </script>
 
